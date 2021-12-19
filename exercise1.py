ee41c0812dfc417b3e039fcbbd771f6ae0b1778b
eclypsis <32039575+eclypsis@users.noreply.github.com>
2021-12-20 00:04:30 +0200

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,9 +1,8 @@
+
+
 list = ["pizza", "loukoumades", "melomakarona", "kourampiedes", "tzatziki", "paidakia"]
-for item in list:
+for x in list:
+    print(x)
+    Total_number_of_items = len(list)
 
-print(item)
-
-Total_number_of_items = len(list)
 print("Total number of items: ", Total_number_of_items )
-
-
